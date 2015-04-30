d95413351d41f7d6092f1644eb3619a531654c63
kapadiamush <kapadiamush@gmail.com>
2015-04-30 19:11:00 -0400

    initial commit

---
+++ js/router.js
@@ -0,0 +1,23 @@
+var Router = Backbone.Router.extend({
+    routes: {
+        '' : 'home',
+        'landlords' : 'landlords',
+        'students': 'students'
+    }
+});
+
+var router = new Router(); 
+
+router.on('route:home', function(){
+    console.log("loading homepage"); 
+});
+
+router.on('route:landlords', function(){
+    landlordList.render();
+}); 
+
+router.on('route:students', function(){
+    console.log("loading students");
+});
+
+Backbone.history.start({pushState: true}); 
